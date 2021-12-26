a5579c8ac24af3ba93348f34af2650cede61dc10
Stepan <76563428+Mr-Grag-Universe@users.noreply.github.com>
2021-12-26 12:40:31 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/test_git.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_git.dir/main.c.o"
+  "libtest_git.dll.a"
+  "test_git.exe"
+  "test_git.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
